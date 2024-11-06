c0e5309bb91e40d0744f1022f7bda869b825e111
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2024-11-06 16:56:11 -0300

    Add files via upload

--- atividadePy/atividade.py
+++ atividadePy/atividade.py
@@ -1,6 +1,6 @@
 from pyDatalog import pyDatalog
 
-pyDatalog.create_terms('X,Y,Z,Colaborador, NomeColaborador, Idade, Departamento, NomeProjeto, Projeto, Alocacoes, ParticipandeDe, DepartamentoColaborador, InfoGeral')
+pyDatalog.create_terms('X,Y,Z,Colaborador, NomeColaborador, Idade, Departamento, Projeto, NomeProjeto, Alocacoes, ParticipanteDe, DepartamentoColaborador, InfoGeral, Senior, Quantidade_seniores')
 
 with open('colaboradores.txt') as file:
     for line in file:
@@ -17,33 +17,38 @@ with open('alocacoes.txt') as file:
         nomeColaborador, projeto = line.strip().split(',')
         +Alocacoes(nomeColaborador, projeto)
 
-ParticipanteDe = Alocacoes(NomeColaborador, Projeto)
-print(ParticipanteDe)
+#ATIVIDADE1
 
-print('\n///////////////\n')
+# print("Colaborador")
+# for n,i,d in Colaborador(NomeColaborador, Idade, Departamento):
+#     print(n)
 
-DepartamentoColaborador(NomeColaborador, Departamento) <= Colaborador(NomeColaborador, Idade, Departamento)
-
-print(DepartamentoColaborador(NomeColaborador,Departamento))
-
-print('\n///////////////\n')
-
-InfoGeral(NomeColaborador, Projeto, Departamento) <= Colaborador(NomeColaborador, Projeto, Departamento) 
-
-print(InfoGeral(X,Y,Z))
+# print('\n/////////////////////////\n')
+# print("Projeto")
+# for n, d in Projeto(NomeProjeto, Departamento):
+#     print(n)
 
 
+#ATIVIDADE 2
+
+#ParticipanteDe(NomeColaborador, NomeProjeto) <= Alocacoes(NomeColaborador, NomeProjeto)
+
+#print(ParticipanteDe(X, Y))
+
+# DepartamentoColaborador(NomeColaborador, Departamento) <= Colaborador(NomeColaborador, Idade, Departamento)
+# # print(DepartamentoColaborador(X,Y))
+
+# #InfoGeral(NomeColaborador, NomeProjeto, Departamento) <= ParticipanteDe(NomeColaborador, NomeProjeto) & DepartamentoColaborador(NomeColaborador, Departamento)
+# #print(InfoGeral(X,Y,Z))
 
 
+# #ATIVIDADE 3
 
-'''
-print("Colaboradores:\n")
-for n, i, d in Colaborador(NomeColaborador, Idade, Departamento):
-    print(n)
+# Senior(NomeColaborador) <= Colaborador(NomeColaborador, Idade, Departamento) & (Idade>"30")
 
-print('\n//////////////////////////////\n')
+# # for nomeColaborador, departamento in DepartamentoColaborador(X,Y) & Senior(X):
+# #     print(f'{nomeColaborador} - {departamento}')
 
-print('Projetos: \n')
-for n, d in Projeto(NomeProjeto, Departamento):
-    print(n)
-'''
+# for departamento in DepartamentoColaborador(X,Y) & Senior(X):
+#     quantidade_seniores = len(Senior(X) & DepartamentoColaborador(X,Y))
+#     print(f'{departamento} - {quantidade_seniores}')
